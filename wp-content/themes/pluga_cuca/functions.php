c7e3a21d4a598c8f969ba038adb15bf06419f213
Mateus <mateus21mantovani@hotmail.com>
2018-07-05 17:47:18 -0300

    adicionando bootstrap e ajustes de tema

--- wp-content/themes/pluga_cuca/functions.php
+++ wp-content/themes/pluga_cuca/functions.php
@@ -120,42 +120,20 @@ add_action( 'widgets_init', 'pluga_cuca_widgets_init' );
  * Enqueue scripts and styles.
  */
 function pluga_cuca_scripts() {
-	wp_enqueue_style( 'pluga_cuca-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'bootstrap', get_stylesheet_directory_uri() . '/css/bootstrap.min.css', array(), '4.1.1' );
 
-	wp_enqueue_script( 'pluga_cuca-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'jquery', get_template_directory_uri() . '/js/jquery-3.3.1.slim.min.js', array(), '3.3.1', true );
 
-	wp_enqueue_script( 'pluga_cuca-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+	wp_enqueue_script( 'popper', get_template_directory_uri() . '/js/popper.min.js', array(), false, true );
 }
 add_action( 'wp_enqueue_scripts', 'pluga_cuca_scripts' );
 
-/**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
- * Custom template tags for this theme.
- */
-require get_template_directory() . '/inc/template-tags.php';
 
 /**
  * Functions which enhance the theme by hooking into WordPress.
  */
 require get_template_directory() . '/inc/template-functions.php';
 
-/**
- * Customizer additions.
- */
-require get_template_directory() . '/inc/customizer.php';
 
-/**
- * Load Jetpack compatibility file.
- */
-if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
-}
-
+//remove admin bar
+add_filter('show_admin_bar', '__return_false');
